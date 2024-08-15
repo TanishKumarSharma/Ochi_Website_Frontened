cb0354d83d5eb2aa31f9609197772986904f06e3
Tanish Kumar <tanishkumar.12b2.50@gmail.com>
2024-08-15 14:47:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className='bg-red-400 text-3xl text-white'>Hello</h1>
+    </>
+  )
+}
+
+export default App
